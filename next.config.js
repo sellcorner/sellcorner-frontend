9ea1a89a77ef46485a70addfe090d288902e65b1
SellCorner <sellcorner.net@gmail.com>
2025-06-16 21:44:23 +0600

    fix: add basic next.config.js setup

--- next.config.js
+++ next.config.js
@@ -1,1 +1,8 @@
-next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Add any other Next.js config options you need here
+}
+
+module.exports = nextConfig
+
