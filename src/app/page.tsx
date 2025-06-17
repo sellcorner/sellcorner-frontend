372e8ee2192c9c0b3cd9a1432924cc6e41b346ce
SellCorner <sellcorner.net@gmail.com>
2025-06-17 21:22:52 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
-'use client'
-import HeroSection from '@/components/HeroSection'
-import LatestPosts from '@/components/LatestPosts'
-import LatestProducts from '@/components/LatestProducts'
-export default function Home() {
+import { ReactNode } from "react";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function PostPage({ params }: Props) {
+  const { slug } = params;
+
+  // Use the slug to fetch data or display content
   return (
-    <>
-      <HeroSection />
-      <LatestPosts />
-      <LatestProducts />
-    </>
-  )
+    <main>
+      <h1>Post: {slug}</h1>
+      {/* You can fetch the post content here or use a component */}
+    </main>
+  );
 }
